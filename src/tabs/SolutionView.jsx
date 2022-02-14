1dd6bf67083274a4214516f0dc499738f1c39035
EchoAlice <echoalice0@gmail.com>
2022-02-14 14:15:24 -0500

    Displays anwsers to AOC (minimal viable product)

--- src/tabs/SolutionView.jsx
+++ src/tabs/SolutionView.jsx
@@ -4,7 +4,12 @@ const SolutionView = () => {
   return(    
     // Import from the solution file!
     <div>
-      Insert AOC solutions here!
+      <p>Day 1 (part 1): 1711</p>
+      <p>Day 1 (part 2): 1743</p>
+      <p>Day 2 (part 1): 1383564</p>
+      <p>Day 2 (part 2): 1488311643</p>
+      <p>Day 3 (part 1): 2972336</p>
+      <p>Day 3 (part 2): 3368358</p>
     </div>
   );
 }
