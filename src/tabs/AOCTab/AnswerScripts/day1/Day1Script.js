4832594086efeca3b6bbfb3fb7a4a185c08e2438
EchoAlice <echoalice0@gmail.com>
2022-02-21 19:14:57 -0500

    Imports input file into function, parses string into substrings

--- src/tabs/AOCTab/AnswerScripts/day1/Day1Script.js
+++ src/tabs/AOCTab/AnswerScripts/day1/Day1Script.js
@@ -1,10 +1,22 @@
-const Day1Solution = () => {
+const Day1Solution = (input) => {
+  //const answer ="Day 1 answer goes here" + input
   
+  // Parses file, separating string into individual values
+  const values = input.split("\r\n");
+  console.log(values)
+  
+  const answer = values[0];
   return( 
-    // JavaScript goes here
-    // Fetch the file
-    // Parse the file
+    // This will be a string!
+    answer
   )
 }
 
 export default Day1Solution;
+
+
+/** 
+ * Possibly how convert string into array of strings
+ *    const values = input.split(" ")
+ *    answer = String.valueOf(answer)
+ */
---
+++ src/tabs/AOCTab/AnswerScripts/day3/Day3Script.js
@@ -0,0 +1,12 @@
+const Day3Solution = (input) => {
+  // JavaScript goes here
+  // Fetch the file
+  // Parse the file
+  const answer ="Day 3 answer goes here" + input
+  
+  return( 
+    answer
+  )
+}
+
+export default Day3Solution;
