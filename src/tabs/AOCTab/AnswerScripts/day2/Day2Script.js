ad013590119bd2b9779015cf6cf93fee9f8e276a
EchoAlice <echoalice0@gmail.com>
2022-02-21 19:14:57 -0500

    Imports input file into function, parses string into substrings

---
+++ src/tabs/AOCTab/AnswerScripts/day2/Day2Script.js
@@ -0,0 +1,12 @@
+const Day2Solution = (input) => {
+  // JavaScript goes here
+  // Fetch the file
+  // Parse the file
+  const answer ="Day 2 answer goes here" + input
+  
+  return( 
+    answer
+  )
+}
+
+export default Day2Solution;
--- src/tabs/AOCTab/SolutionView.jsx
+++ src/tabs/AOCTab/SolutionView.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import day1text from './AnswerScripts/Day1Script.jsx.txt';
-import day2text from './AnswerScripts/Day2Script.jsx.txt';
-import day3text from './AnswerScripts/Day3Script.jsx.txt';
+
+/** Import dynamically to reduce the nonsense that is occuring */
+import day1text from './AnswerScripts/day1/Day1Script.txt';
+import day2text from './AnswerScripts/day2/Day2Script.txt';
+import day3text from './AnswerScripts/day3/Day3Script.txt';
+import day1input from './AnswerScripts/day1/input_test.txt';
+import day2input from './AnswerScripts/day2/input_test.txt';
+import day3input from './AnswerScripts/day3/input_test.txt';
+import day1solution from './AnswerScripts/day1/Day1Script.js';
+import day2solution from './AnswerScripts/day2/Day2Script.js';
+import day3solution from './AnswerScripts/day3/Day3Script.js';
 import { CodeBlock, ocean } from "react-code-blocks";
+
 /**
  * When button clicked, key of file gets inserted in as a prop
  * This component takes in prop.day, indexing which file to open and display.
  * Inserts file reference into function that reads in script to a tag
  */
 
-const scripts = [
+const txt_scripts = [
   day1text, 
   day2text, 
-  day3text, 
+  day3text 
 ];
+
+const inputs = [
+  day1input,
+  day2input,
+  day3input
+]
+
+/** Declares array of functions */
+let solutions = [day1solution, day2solution, day3solution]
 
 /**
  * When specific button is clicked, pull up specific solution
@@ -21,25 +39,52 @@ const scripts = [
  */
 const SolutionView = ({ day }) => {
 
-// Declare state variable that will be the file
-const [file, setFile] = useState("");  
-const script = scripts[day - 1]
+/** Declare hooks */ 
+const [script_file, setScriptFile] = useState("");  
+const [input_file, setInputFile] = useState("");  
 
+/** Declare state variables that will be the files imported */ 
+const index = [day - 1]
+const txt_script = txt_scripts[index]
+const input = inputs[index]
+
+/** 
+ * Fetches the script display that user wants to see
+ * Should I fetch the input.txt file in the same useEffect?
+ * Then pass that in as a parameter to Day_Script.js
+ */ 
 useEffect(() => {
-  fetch( script )
+  fetch( txt_script )
     .then(r => r.text())
-    .then(file => setFile(file))
-})
+    .then(script_file => setScriptFile(script_file))
 
-// Fetch the input
+  fetch( input )
+    .then(s => s.text())
+    .then(input_file => setInputFile(input_file))
+  })
+
+/**
+ * Bug here. I think the page doesn't render at all if I have this conditional
+ * statement wrapped around the entire return statement. 
+ * Figure out how to protect code from getting passed in an empty string! 
+ * if (input_file !== "" && solutions[index](input_file) !== ""){}
+ */
 
 return(    
-  <CodeBlock
-    text={file}
-    language="javascript"
-    theme={ocean}
-  />
-);
+  <>
+    {/* Displays the script  */}
+    <CodeBlock
+      text={script_file}
+      language="javascript"
+      theme={ocean}
+    />
+
+    {/* Passes in the input file into the javascript file  */}
+    <div>
+      answer={ solutions[index](input_file) }
+    </div> 
+  </>
+  );
 };
 
 export default SolutionView;
