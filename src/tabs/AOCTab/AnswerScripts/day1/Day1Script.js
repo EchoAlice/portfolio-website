f2c61c996f712a7d11e8ff814290d27e751e171e
EchoAlice <echoalice0@gmail.com>
2022-02-23 13:47:14 -0500

    Executes and displays Day 2 (part 1) AOC

--- src/tabs/AOCTab/AnswerScripts/day1/Day1Script.js
+++ src/tabs/AOCTab/AnswerScripts/day1/Day1Script.js
@@ -1,23 +1,10 @@
-const Day1Solution = (input) => {
-  
-  // Parses file, separating string into individual values
-  const depths = input.split("\r\n");
-  
-  let previous_element = 0;
-  let depth_increases = 0;
+function Day1Solution(input) {
 
-  for (let i = 0; i < depths.length; i++) {
-    const depth_calculation = depths[i] - previous_element;
-    if (depth_calculation > 0 && previous_element !== 0) {
-      depth_increases = depth_increases + 1;
-    }
-    previous_element = depths[i]; 
-  }
+  const answer = "Day 1 answer goes here";
 
-  const answer = depth_increases
-  return( 
+  return (
     answer
-  )
+  );
 }
 
 export default Day1Solution;
