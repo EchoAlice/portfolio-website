105024927501f51a4f3c128b6ecf8fff1bde9cb5
EchoAlice <echoalice0@gmail.com>
2022-04-06 12:43:02 -0400

    Dynamically creates dropdown items based on number of days completed

--- src/tabs/AOCTab/SolutionsView.jsx
+++ src/tabs/AOCTab/SolutionsView.jsx
@@ -2,23 +2,38 @@ import React, { useState } from 'react';
 import SolutionView from './SolutionView';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 
-// Create a dropdown menu for the days completed.  
-// This menu will give user choice of which solution to view
+/*
+ * This function is pretty dang complex...
+ * Does the variable day in the for loop need to
+ * be called something else?
+ */
 
-// let numberOfDays = 4;
+const dropdownItems = (numberOfItems, setDay) => {
+  const items = [];
+  for (let day = 1; day <= numberOfItems; day++) {
+    items.push(
+      <Dropdown.Item onClick={() => {setDay(day)}}>
+        Day {day}
+      </Dropdown.Item>
+    );
+  }
+  return items;
+};
+
+/*
+ * Creates a dropdown menu for the days completed
+ */
 
 const SolutionsView = () => {
   const [day, setDay] = useState("");
+  var numberOfDays = 4;
   // Do I need to return <SolutionView /> that passes in the day that is clicked?
   
   return( 
     // Idk if setDay should be here. I have it in each dropdown item
     <>
       <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-        <Dropdown.Item onClick={() => setDay(1)} href="#/action-1">Day 1</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDay(2)} href="#/action-2">Day 2</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDay(3)} href="#/action-2">Day 3</Dropdown.Item>
-        <Dropdown.Item onClick={() => setDay(4)} href="#/action-2">Day 4</Dropdown.Item>
+        { dropdownItems(numberOfDays, setDay) }
       </DropdownButton>      
       
       {/* Pass in the day to the solutionView tag */}
@@ -28,24 +43,3 @@ const SolutionsView = () => {
 }
 
 export default SolutionsView;
-
-
-
-
-  // Creates items within a loop instead of hard coding all
-
-  // const dropdownItems = (numberOfDays) => {
-  //   for (let i = 1; i <= numberOfDays; i++) {
-  //     <Dropdown.Item onClick={() => setDay(i)} href="#/day"i>Day {i}</Dropdown.Item>
-  //   }
-  //   return( 
-  //     "How do I return all buttons?"
-  //   )
-  // }
- 
-  
-  // return (
-  //   <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-  //     {/* { dropdownItems(4, setDay) }  */}
-  //   </DropdownButton>
-  // )
