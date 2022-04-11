51c5fdb6a479f286379724907ed300f63f5a4a87
EchoAlice <echoalice0@gmail.com>
2022-04-11 11:21:26 -0400

    Creates script.txt files and displays them on site based on day

--- src/tabs/AOCTab/SolutionView.jsx
+++ src/tabs/AOCTab/SolutionView.jsx
@@ -4,9 +4,12 @@ import day1input from "./days/day1/input.txt";
 import day2input from "./days/day2/input.txt";
 import day3input from "./days/day3/input.txt";
 import day4input from "./days/day4/input.txt";
-// import { CodeBlock, ocean } from "react-code-blocks";
+import day1script from "./days/day1/day1script.txt";
+import day2script from "./days/day2/day2script.txt";
+import day3script from "./days/day3/day3script.txt";
+import day4script from "./days/day4/day4script.txt";
+import { CodeBlock, ocean } from "react-code-blocks";
 
-// Is there any way to dynamically load input?
 const inputs = [
   day1input,
   day2input,
@@ -14,8 +17,16 @@ const inputs = [
   day4input
 ]
 
+const scripts = [
+  day1script,
+  day2script,
+  day3script,
+  day4script
+]
+
 /*
- *Dynamcally imports Component based on state
+ * Dynamcally imports Component based on state.  Figure out how to
+ *    do this with input.txt and script.txt files
  */
 
  function loadComponent(day) {
@@ -27,6 +38,7 @@ const inputs = [
 
 const SolutionView = (props) => {
   const [input, setInput] = useState("");
+  const [script, setScript] = useState("");
   const day = props.parameter;
   const index = day - 1;
   const Component = loadComponent(day); 
@@ -35,6 +47,11 @@ const SolutionView = (props) => {
     fetch(inputs[index])
       .then(r => r.text())
       .then(input => setInput(input))
+    
+    fetch(scripts[index])
+      .then(r => r.text())
+      .then(script => setScript(script))
+      console.log(script)
   })
 
   // How do I use CodeBlock now that we have these new tags?
@@ -43,6 +60,14 @@ const SolutionView = (props) => {
       <Suspense fallback={<div>Loading...</div>}>
         <ErrorBoundary>
           
+          {/* Displays script */}
+          <CodeBlock
+            text={script}
+            language="javascript"
+            theme={ocean}
+          /> 
+          
+          {/* Displays answer */}
           <Component input={input}/>
         
         </ErrorBoundary>
@@ -52,10 +77,3 @@ const SolutionView = (props) => {
 }
 
 export default SolutionView;
-
-
-
-
-
-
-
