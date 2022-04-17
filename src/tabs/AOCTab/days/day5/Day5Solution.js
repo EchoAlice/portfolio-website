64e3ef987dc00a36dffe7cd906053174ecc3d45d
EchoAlice <echoalice0@gmail.com>
2022-04-16 21:10:59 -0400

    Progress on Day 5 part 1

--- src/tabs/AOCTab/days/day5/Day5Solution.js
+++ src/tabs/AOCTab/days/day5/Day5Solution.js
@@ -1,3 +1,6 @@
+// Input for day 1 gets passed in as input here before day 5's
+// This is a problem
+
 const cleanLine = (inputLine) => {
   // Seperate line into the 4 numbers they consist of.
   const numbers = inputLine.split(/(?:,| )+/);
@@ -5,14 +8,15 @@ const cleanLine = (inputLine) => {
     if (numbers[i] === "->") {
       numbers.splice(i,1);
     }
-  console.log("numbers in a line: " + numbers[i])
+  // console.log(i + "th number: " + numbers[i])
   }
   return numbers
 }
 
-/* Only horizontal and vertical lines are recorded */
+/* 
+ * Only horizontal and vertical lines are recorded
+ */
 const checkLineDirection = (numbers) => {
-  
   const x1 = numbers[0];
   const y1 = numbers[1];
   const x2 = numbers[2];
@@ -26,26 +30,86 @@ const checkLineDirection = (numbers) => {
   }
 }
 
+/*
+ * We want to fill in points between the ends of coordinates
+ */
+const createLine = (endPoints) => {
+  // endPoints is already an array!!!
+  console.log("End Points: " + endPoints[0]);
+   
+  const x1 = endPoints[0];
+  const y1 = endPoints[1];
+  const x2 = endPoints[2];
+  const y2 = endPoints[3];
+  const line = []; 
+
+  const vertical_distance = Math.abs(y1 - y2); 
+  const horizontal_distance = Math.abs(x1 - x2); 
+  // const line_length = vertical_distance + horizontal_distance;
+  
+  // This feels extremely repetetive... 
+  // Copy x coordinate and add/subtract from 
+  // y depending on which y is first
+  if (horizontal_distance === 0) {
+    if (y1 > y2) {
+      for (let i = y2; y2 < y1; i++) {
+        line.push(x1, i);
+      }
+    }
+    if (y1 < y2) {
+      for (let i = y1; y1 < y2; i++) {
+        line.push(x1, i);
+      }
+    }
+  }
+  // Copy y coordinate and add/subtract from 
+  // x depending on which x is first
+  if (vertical_distance === 0) {
+    if (x1 > x2) {
+      for (let i = x2; x2 < x1; i++) {
+        line.push(i, y1);
+      }
+    }
+    if (x1 < x2){
+      for (let i = x1; x1 < x2; i++) {
+        line.push(i, y1);
+      }
+    }
+  }
+  console.log("Full Line: " + line);
+}
+
+
 const Day5Solution = (props) => {
   // Turn input into lines
   const input = props.input;
   const lines = input.split("\r\n");
 
-  var horv = 0;
-  
-  const numberOfLines = lines.length;
-  for (let i = 0; i < numberOfLines; i++) {
-    const lineToCheck = cleanLine(lines[i]);
-    const direction = checkLineDirection(lineToCheck);
+  var horiz_or_vert = 0;
+  const number_of_lines = lines.length;
+  // const vents = [];
+
+  for (let i = 0; i < number_of_lines; i++) {
+    console.log("\n")
+    console.log("Line Number: " + i);
+    console.log("Line to clean: " + lines[i]);
+    const end_points = cleanLine(lines[i]);
+    const direction = checkLineDirection(end_points);
     // What calculation do i need to make when direction is h or v?
-    // And why is horv 6 and not 7?? buggy buggy
+    // console.log(`Line ${i} counts: ` + direction); 
+    
     if (direction === true) {
-      console.log("Thats da truuf!");
-      horv++;
+      const points_of_line = createLine(end_points);
+      console.log(points_of_line); 
+      // How do we append points of a line to vents array?
+      
+      horiz_or_vert++;
     } 
   }
-  
-  const answer = horv;
+ 
+  // const pointsOverlap = addUpVentOverlap(pointsOfLine);
+  // const answer = pointsOverlap 
+  const answer = horiz_or_vert;
   return(
     "Answer: " + answer
   )
