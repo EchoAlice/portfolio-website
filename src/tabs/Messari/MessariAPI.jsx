99e42787410a3425b7eed3e82aab8518fe9516c2
EchoAlice <echoalice0@gmail.com>
2022-04-18 15:31:49 -0400

    Creates button that fetches BTC price API when clicked

--- src/tabs/Messari/MessariAPI.jsx
+++ src/tabs/Messari/MessariAPI.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
+
 /*
  * Pulls data from Messari's API and displays it on website
  */
 const MessariAPI = (props) => {
-  const whatUseEffectDependsOn = props.state
+  const count = props.count;
   const [apiIsLoading, setApiIsLoading] = useState(true);
   const [price, setPrice] = useState("");
   const url = "https://data.messari.io/api/v1/assets/btc/metrics"; 
-  
   useEffect(() => {
     setApiIsLoading(true);
     fetch(url)
@@ -17,7 +17,7 @@ const MessariAPI = (props) => {
         setPrice(response.data.market_data.price_usd);
         setApiIsLoading(false);
       })
-  }, [whatUseEffectDependsOn]);
+  }, [count]);
 
   return(    
     <>
