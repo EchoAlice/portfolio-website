ad346a67bdba93b12c1a213a248b7dc23b415c6c
EchoAlice <echoalice0@gmail.com>
2022-04-18 15:31:49 -0400

    Creates button that fetches BTC price API when clicked

--- src/tabs/BitcoinPriceTab.jsx
+++ src/tabs/BitcoinPriceTab.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MessariAPI from './Messari/MessariAPI';
 
+/*
+ * Displays BTC price and re-renders API when button is clicked
+ */
 const BitcoinPriceTab = () => {
+  const [count, setCount] = useState(0);
   return(    
-    <div>
-      <MessariAPI/>
-    </div>
+    <>
+      <div>BTC Price USD: </div>
+      <MessariAPI count={ count }/>
+      <button onClick={() => setCount(count + 1)}>
+        Update BTC Price
+      </button>
+    </>
   );
 }
 
