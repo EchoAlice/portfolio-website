2cc5c5b8e0298579d61677fd22dc1262c3d2e889
EchoAlice <echoalice0@gmail.com>
2022-04-18 12:42:50 -0400

    Displays bitcoin's price, pulled from Messari's API

--- src/tabs/Messari/MessariAPI.jsx
+++ src/tabs/Messari/MessariAPI.jsx
@@ -5,27 +5,24 @@ import React, { useEffect, useState } from 'react';
 const MessariAPI = (props) => {
   const whatUseEffectDependsOn = props.state
   const [apiIsLoading, setApiIsLoading] = useState(true);
-  const url = "https://data.messari.io/api/v1/assets/btc/profile"; 
+  const [price, setPrice] = useState("");
+  const url = "https://data.messari.io/api/v1/assets/btc/metrics"; 
   
-  // This is working.  Response returns the api object.
-  // Now, figure out how to parse the object to display what I want 
-  // to be displayed.
   useEffect(() => {
     setApiIsLoading(true);
     fetch(url)
       .then(r => r.json())
       .then (response => {
         console.log(response);
+        setPrice(response.data.market_data.price_usd);
         setApiIsLoading(false);
       })
   }, [whatUseEffectDependsOn]);
 
-  // If api is not loading, return compoinent, else return "loading"
   return(    
     <>
-      {!apiIsLoading ? (
-        <div>Messari api data here:</div>
-        // <MessariAPI/>
+      {!apiIsLoading && price !== "" ? (
+        <div>{price}</div>
       ) : (
         <div> Loading... </div>
       )}
