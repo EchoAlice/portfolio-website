aff6dc8537db487fa6c3010369547fc0198d6fb4
EchoAlice <echoalice0@gmail.com>
2022-04-20 12:22:08 -0400

    Tinkering with CSS

--- src/tabs/HomeTab.jsx
+++ src/tabs/HomeTab.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const HomeTab = () => {
   return(    
+  <div class="home-background">  
     <a href='https://giphy.com/clips/hamlet-jJjb9AUHOiP3nJJMdy'rel="noopener noreferrer" target="_blank">Hello!</a>
+  </div>
   );
 }
 
