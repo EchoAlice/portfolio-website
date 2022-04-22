43ca9f8a9d739cbaf4178e43d2a548cb6c80a1a0
EchoAlice <echoalice0@gmail.com>
2022-04-22 11:04:27 -0400

    Addresses all of Leo's corrections to code

--- src/tabs/BTCTab/MessariAPI.jsx
+++ src/tabs/BTCTab/MessariAPI.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
+const url = "https://data.messari.io/api/v1/assets/btc/metrics"; 
+
 /*
  * Pulls data from Messari's API and displays it on website
  */
 const MessariAPI = (props) => {
   const count = props.count;
   const [apiIsLoading, setApiIsLoading] = useState(true);
+  const [apiFailedToLoad, setApiFailedToLoad] = useState(false);
   const [price, setPrice] = useState("");
-  const url = "https://data.messari.io/api/v1/assets/btc/metrics"; 
-  
+ 
   useEffect(() => {
     setApiIsLoading(true);
     fetch(url)
@@ -16,16 +18,23 @@ const MessariAPI = (props) => {
       .then (response => {
         console.log(response);
         setPrice(response.data.market_data.price_usd);
+        price === "" || price === undefined ? (
+        setApiFailedToLoad(true) 
+        ) : ( 
+          setApiFailedToLoad(false)
+        );
         setApiIsLoading(false);
       })
-  }, [count]);
+  }, [count, price]);
 
   return(    
     <>
-      {!apiIsLoading && price !== "" ? (
+      {!apiIsLoading && !apiFailedToLoad && price !== "" ? (
         <div>{price}</div>
-      ) : (
-        <div> Loading... </div>
+      ) : apiIsLoading && !apiFailedToLoad ? (
+        <div> Loading... </div> 
+      ) : ( 
+       <div> Failed to load</div> 
       )}
     </>
   );
