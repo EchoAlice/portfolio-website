0ba9e2dce9a7d832f78cc282ec99cf420905c97b
EchoAlice <echoalice0@gmail.com>
2022-04-22 13:55:48 -0400

    Displays and executes both parts of AOC's Day 2

--- src/tabs/AOCTab/days/day2/Day2Solution.js
+++ src/tabs/AOCTab/days/day2/Day2Solution.js
@@ -13,19 +13,49 @@ const Day2Solution = (props) => {
     // Arithmetic opperation applied depending on the direction given
     if (direction === "forward")
       horizontal_position += value;
-      console.log(horizontal_position)
     if (direction === "up")
       sub_depth -= value;
-      console.log(sub_depth)
     if (direction === "down")
       sub_depth += value;
-      console.log(sub_depth)
     }
   
-  const answer = sub_depth * horizontal_position
+  const part1_answer = sub_depth * horizontal_position;
   
-  return( 
-    "Answer: " + answer 
+/********************************************************
+                        Part 2 
+ ********************************************************/  
+ 
+  let horizontal_position_p2 = 0;
+  let sub_depth_p2 = 0;
+  let aim = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    // Separates each value into another array to evaluate string before numeric value 
+    const split_value = values[i].split(" ");
+    const direction =  split_value[0];
+    const value = parseInt(split_value[1]);
+    if (direction === "forward") {
+      horizontal_position_p2 += value;
+      sub_depth_p2 += aim * value;
+    }
+    if (direction === "up") {
+      aim -= value;
+    }
+    if (direction === "down") {
+      aim += value;
+    }
+  }
+  const part2_answer = horizontal_position_p2 * sub_depth_p2;
+
+  return(
+    <>
+      <div>
+        Part 1 Answer: {part1_answer} 
+      </div>
+      <div>
+        Part 2 Answer: {part2_answer}  
+      </div>
+    </>
   )
 }
 
