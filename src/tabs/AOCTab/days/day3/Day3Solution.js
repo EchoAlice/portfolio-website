be6e37844df3bb85fcc29390b2336914f8ffa90c
EchoAlice <echoalice0@gmail.com>
2022-04-23 14:11:59 -0400

    WIP

--- src/tabs/AOCTab/days/day3/Day3Solution.js
+++ src/tabs/AOCTab/days/day3/Day3Solution.js
@@ -38,10 +38,75 @@ const Day3Solution = (props) => {
   const gamma = parseInt(binary_input_gamma, 2);
   const epsilon = parseInt(binary_input_epsilon, 2 );
 
-  const answer = gamma * epsilon;
+  const part1_answer = gamma * epsilon;
+ 
+ /********************************************************
+                        Part 2 
+ ********************************************************/ 
+  let generator = 0;
+  let scrubber = 0;
+  let zero_bits = 0;
+  let one_bits = 0;  
+  let generator_bit_criteria = 0;
+  let scrubber_bit_criteria = 0;
+
+  console.log("\n");
+  
+  const generatorBitCriteria = (zero_bits, one_bits) => {
+    if (zero_bits > one_bits) {
+      generator_bit_criteria = 0;
+      return 0;
+    }
+    if (zero_bits <= one_bits) {
+      generator_bit_criteria = 1;
+      return 1;
+    }
+  }
+  
+  const scrubberBitCriteria = (zero_bits, one_bits) => {
+    if (zero_bits > one_bits) {
+      scrubber_bit_criteria = 1;
+      return 1;
+    }
+    if (zero_bits <= one_bits) {
+      scrubber_bit_criteria = 0;
+      return 0;
+    }
+  }
+
+  // Records the number of types of bits within a column
+  // Put this inside larger for loop
+  for (let h = 0; h < height; h++) {
+    console.log(`Bit: ${binary_input[h][0]}`);
+    if (binary_input[h][0] === '0') {
+      zero_bits += 1;
+    }
+    else {
+      one_bits += 1;
+    }
+  } 
+  // Gen and scrub functions work!
+  generator_bit_criteria = generatorBitCriteria(zero_bits, one_bits); 
+  scrubber_bit_criteria = scrubberBitCriteria(zero_bits, one_bits); 
+
+  console.log("Generator bit criteria: " + generator_bit_criteria);
+  console.log("Scrubber bit criteria: " + scrubber_bit_criteria);
+  console.log(`0 bits: ${zero_bits}`);
+  console.log(`1 bits: ${one_bits}`);
+
+  // binary_input works, but are all strings!
+  const life_support_rating = generator * scrubber;  
+  const part2_answer = life_support_rating; 
   return( 
-    "Answer: " + answer
-  )
+    <>
+      <div>
+        Part 1 Answer: {part1_answer} 
+      </div>
+      <div>
+        Part 2 Answer: {part2_answer}  
+      </div>
+    </> 
+    )
 }
 
 export default Day3Solution
