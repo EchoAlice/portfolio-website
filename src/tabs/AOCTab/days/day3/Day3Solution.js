37195e4128ed311f3bae3c348f2485e0efeb4312
EchoAlice <echoalice0@gmail.com>
2022-04-30 14:31:06 -0400

    Address Leo's comments

--- src/tabs/AOCTab/days/day3/Day3Solution.js
+++ src/tabs/AOCTab/days/day3/Day3Solution.js
@@ -1,40 +1,32 @@
 const Day3Solution = (props) => {
     // Parses file, separating string into individual values
   const input = props.input
-  var binary_input = input.split("\r\n");
+  let binary_input = input.split(/\r?\n/);
 
   // Lengths of binary digits and array
-  const width = binary_input[0].length; 
-  const height = binary_input.length;
+  let row = binary_input.length;
+  let column = binary_input[0].length; 
 
   let binary_input_gamma = "";
   let binary_input_epsilon = "";
 
-  for (let c = 0; c < width; c++) {
-    /**
-     * Find most common bit in gamma rate
-     * Access each individual value in list
-     */
+  for (let c = 0; c < column; c++) {
+    //Find most common bit in gamma rate.  Access each individual value in list
     let count_0 = 0;
     let count_1 = 0;
-    /**
-     * Calculates the number of times first bit of each number is present 
-     */ 
-    for (let r = 0; r < height; r++) {
-      parseInt(binary_input[r][c]) === 0 ? count_0 += 1 : count_1 += 1;
-      parseInt(binary_input[r][c]) === 1 ? count_1 += 1 : count_0 += 1;
+    
+    // Calculates the number of times first bit of each number is present 
+    for (let r = 0; r < row; r++) {
+      parseInt(binary_input[r][c], 2) === 0 ? count_0 += 1 : count_1 += 1;
+      parseInt(binary_input[r][c], 2) === 1 ? count_1 += 1 : count_0 += 1;
     }
 
-    /**
-     * Adds 0 or 1 to gamma and epsilon depending on which bit shows up more
-     */
+    // Adds 0 or 1 to gamma and epsilon depending on which bit shows up more
     count_0 > count_1 ? binary_input_gamma += "0" : binary_input_gamma += "1";
     count_0 > count_1 ? binary_input_epsilon += "1" : binary_input_epsilon += "0";
   }
 
-  /**
-   * Convert binary to integer
-   */
+  // Convert binary to integer
   const gamma = parseInt(binary_input_gamma, 2);
   const epsilon = parseInt(binary_input_epsilon, 2 );
 
@@ -44,35 +36,32 @@ const Day3Solution = (props) => {
                         Part 2 
  ********************************************************/ 
   let bit_criteria = 0;
-  var row_p2 = binary_input.length;
-  let column_p2 = binary_input[0].length;
   let binary_input_copy = [];
   let answer = [];
-  
-  const generatorBitCriteria = (zero_bits, one_bits) => {
-    if (zero_bits > one_bits) {
+ 
+  // We are updating the bit criteria within these functions, without returning the values
+  // These next 2 functions are creating side effects
+  const createGeneratorBitCriteria = (num_of_zero_bits, num_of_one_bits) => {
+    if (num_of_zero_bits > num_of_one_bits) {
       bit_criteria = 0;
       return "0";
     }
-    if (zero_bits <= one_bits) {
+    else {
       bit_criteria = 1;
       return "1";
     }
   }
-  const scrubberBitCriteria = (zero_bits, one_bits) => {
-    if (zero_bits > one_bits) {
+  const createScrubberBitCriteria = (num_of_zero_bits, num_of_one_bits) => {
+    if (num_of_zero_bits > num_of_one_bits) {
       bit_criteria = 1;
       return "1";
     }
-    if (zero_bits <= one_bits) {
+    else {
       bit_criteria = 0;
       return "0";
     }
   }
-  const winningNumber = (final_string) => {
-    let winner = parseInt(final_string, 2);
-    return winner;
-  }
+  const winningNumber = (final_string) => parseInt(final_string, 2);
   const createBinaryCopy = (binary_input) => {
     for (let r = 0; r < binary_input.length; r++) {
       binary_input_copy[r] = binary_input[r];
@@ -83,13 +72,13 @@ const Day3Solution = (props) => {
   // Cycles through each bit criteria and input array
   for (let a = 0; a < 2; a++) { 
     binary_input_copy = createBinaryCopy(binary_input); 
-    row_p2 = binary_input_copy.length; 
+    row = binary_input_copy.length; 
     // Accesses each element in binary input 
-    for (let c = 0; c < column_p2; c++) { 
+    for (let c = 0; c < column; c++) { 
       let zero_bits = 0;
       let one_bits = 0;  
       // Records the number of each type of bit within a column
-      for (let r = 0; r < row_p2; r++) {
+      for (let r = 0; r < row; r++) {
         let bit_in_question = binary_input_copy[r][c];  
         if (bit_in_question === '0') {
           zero_bits += 1;
@@ -100,20 +89,20 @@ const Day3Solution = (props) => {
       }
       // Generates bit criteria based on cycle 
       if (a === 0){
-        bit_criteria = generatorBitCriteria(zero_bits, one_bits); 
+        bit_criteria = createGeneratorBitCriteria(zero_bits, one_bits); 
       }
       else {
-        bit_criteria = scrubberBitCriteria(zero_bits, one_bits); 
+        bit_criteria = createScrubberBitCriteria(zero_bits, one_bits); 
       } 
       // Compares each bit in question to criteria and deletes number from array   
-      for (let r = row_p2 - 1; r >= 0; r--) {
-        let bit_in_question = binary_input_copy[r][c]; 
-        if (bit_in_question !== bit_criteria && row_p2 > 1) {
+      for (let r = row - 1; r >= 0; r--) {
+        let bit = binary_input_copy[r][c]; 
+        if (bit !== bit_criteria && row > 1) {
           binary_input_copy.splice(r, 1); 
         }
       }
-      row_p2 = binary_input_copy.length;
-      if (row_p2 === 1) {
+      row = binary_input_copy.length;
+      if (row === 1) {
         answer[a] = winningNumber(binary_input_copy);
       }
     }
